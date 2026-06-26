2bf1a859c79981a62b708743b728aa1b9fc7d64f
ahmedla775-lab <ahmedla775@gmail.com>
2026-06-27 00:32:39 +0100

    Remove redundant code for card click events

--- dashboard.js
+++ dashboard.js
@@ -1,13 +1,1 @@
 console.log("Dashboard Loaded");
-
-const cards = document.querySelectorAll(".card");
-
-cards.forEach(card => {
-
-  card.addEventListener("click", () => {
-
-    card.classList.toggle("active-card");
-
-  });
-
-});
